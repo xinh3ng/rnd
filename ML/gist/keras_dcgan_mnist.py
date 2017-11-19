d745f4cacace15b75cd13da86ce42f6a4c2bb396
Xin Heng <xin.heng@gmail.com>
2017-11-18 20:08:18 -0800

    Added DCGAN on MNIST

---
+++ ML/gist/keras_dcgan_mnist.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+https://github.com/adeshpande3/Generative-Adversarial-Networks/blob/master/Generative%20Adversarial%20Networks%20Tutorial.ipynb
+"""
+from pdb import set_trace as debug
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape, Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout, BatchNormalization
+from keras.optimizers import RMSprop
+from pydsutils.generic import create_logger
+
+logger = create_logger(__name__)
+
+
+class DCGAN(object):
+    def __init__(self, img_rows=28, img_cols=28, channel=1):
+
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.channel = channel
+        self.d = None   # discriminator
+        self.g = None   # generator
+        self.adv_model = None  # adversarial model
+        self.d_model = None  # discriminator model
+
+    def discriminator(self):
+        """
+        (W−F+2P)/S+1
+        """
+        if self.d:
+            return self.d
+        
+        self.d = Sequential()
+        depth = 64
+        dropout = 0.4
+        # In: 28 x 28 x 1, depth = 1
+        # Out: 14 x 14 x 1, depth = 64
+        input_shape = (self.img_rows, self.img_cols, self.channel)
+        self.d.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape, padding='same'))
+        self.d.add(LeakyReLU(alpha=0.2))
+        self.d.add(Dropout(dropout))
+
+        self.d.add(Conv2D(depth*2, 5, strides=2, padding='same'))
+        self.d.add(LeakyReLU(alpha=0.2))
+        self.d.add(Dropout(dropout))
+
+        self.d.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+        self.d.add(LeakyReLU(alpha=0.2))
+        self.d.add(Dropout(dropout))
+
+        self.d.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+        self.d.add(LeakyReLU(alpha=0.2))
+        self.d.add(Dropout(dropout))
+
+        # Out: 1-dim probability
+        self.d.add(Flatten())
+        self.d.add(Dense(1))
+        self.d.add(Activation('sigmoid'))
+        self.d.summary()
+        return self.d
+    
+    def generator(self):
+        if self.g:
+            return self.g
+        
+        self.g = Sequential()
+        dropout = 0.4
+        depth = 64 + 64 + 64 + 64
+        dim = 7
+        # In: 100
+        # Out: dim x dim x depth
+        self.g.add(Dense(dim*dim*depth, input_dim=100))
+        self.g.add(BatchNormalization(momentum=0.9))
+        self.g.add(Activation('relu'))
+        self.g.add(Reshape((dim, dim, depth)))
+        self.g.add(Dropout(dropout))
+
+        # In: dim x dim x depth
+        # Out: 2*dim x 2*dim x depth/2
+        self.g.add(UpSampling2D())
+        self.g.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
+        self.g.add(BatchNormalization(momentum=0.9))
+        self.g.add(Activation('relu'))
+
+        self.g.add(UpSampling2D())
+        self.g.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
+        self.g.add(BatchNormalization(momentum=0.9))
+        self.g.add(Activation('relu'))
+
+        self.g.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
+        self.g.add(BatchNormalization(momentum=0.9))
+        self.g.add(Activation('relu'))
+
+        # Out: 28 x 28 x 1 grayscale image [0.0, 1.0] per pix
+        self.g.add(Conv2DTranspose(1, 5, padding='same'))
+        self.g.add(Activation('sigmoid'))
+        self.g.summary()
+        return self.g
+    
+    def discriminator_model(self):
+        if self.d_model:
+            return self.d_model
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
+        self.d_model = Sequential()
+        self.d_model.add(self.discriminator())
+        self.d_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return self.d_model
+
+    def adversarial_model(self):
+        if self.adv_model:
+            return self.adv_model
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
+        self.adv_model = Sequential()
+        self.adv_model.add(self.generator())
+        self.adv_model.add(self.discriminator())
+        self.adv_model.compile(loss='binary_crossentropy', optimizer=optimizer,\
+            metrics=['accuracy'])
+        return self.adv_model
+
+
+class DCGAN_Trainer(object):
+    def __init__(self, img_rows, img_cols, channel, 
+                 train_steps, batch_size, save_interval):
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.channel = channel
+        
+        self.train_steps = train_steps
+        self.batch_size = batch_size
+        self.save_interval = save_interval
+
+        self.dcgan = DCGAN()
+        self.discriminator = self.dcgan.discriminator_model()
+        self.generator = self.dcgan.generator()
+        self.adversarial = self.dcgan.adversarial_model()
+
+    def train(self, x_train):
+        
+        noise_input = None
+        if self.save_interval > 0:
+            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        for i in range(self.train_steps):
+            images_train = x_train[np.random.randint(0,
+                x_train.shape[0], size=self.batch_size), :, :, :]
+            noise = np.random.uniform(-1.0, 1.0, size=[self.batch_size, 100])
+            images_fake = self.generator.predict(noise)
+            x = np.concatenate((images_train, images_fake))
+            y = np.ones([2*self.batch_size, 1])
+            y[self.batch_size:, :] = 0
+            d_loss = self.discriminator.train_on_batch(x, y)
+
+            y = np.ones([self.batch_size, 1])
+            noise = np.random.uniform(-1.0, 1.0, size=[self.batch_size, 100])
+            a_loss = self.adversarial.train_on_batch(noise, y)
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            logger.info(log_mesg)
+            if self.save_interval>0:
+                if (i+1) % self.save_interval==0:
+                    self.plot_images(x_train, save2file=True, samples=noise_input.shape[0],\
+                        noise=noise_input, step=(i+1))
+
+    def plot_images(self, x_train, save2file=False, fake=True, samples=16, noise=None, step=0):
+        filename = 'mnist.png'
+        if fake:
+            if noise is None:
+                noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
+            else:
+                filename = "mnist_%d.png" % step
+            images = self.generator.predict(noise)
+        else:
+            i = np.random.randint(0, x_train.shape[0], samples)
+            images = x_train[i, :, :, :]
+
+        plt.figure(figsize=(10,10))
+        for i in range(images.shape[0]):
+            plt.subplot(4, 4, i+1)
+            image = images[i, :, :, :]
+            image = np.reshape(image, [self.img_rows, self.img_cols])
+            plt.imshow(image, cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        if save2file:
+            plt.savefig(filename)
+            plt.close('all')
+        else:
+            plt.show()
+
+#########################################
+if __name__ == '__main__':
+    img_rows = 28
+    img_cols = 28
+    channel = 1
+    train_steps = 2000
+    batch_size = 256
+    save_interval = 0
+                        
+    x_train = input_data.read_data_sets("mnist", one_hot=True).train.images
+    x_train = x_train.reshape(-1, img_rows, img_cols, 1).astype(np.float32)
+
+    trainer = DCGAN_Trainer(img_rows, img_cols, channel, 
+                            train_steps=train_steps, 
+                            batch_size=batch_size, 
+                            save_interval=save_interval)
+    trainer.train(x_train)
+    trainer.plot_images(x_train, fake=True)
+    trainer.plot_images(x_train, fake=False, save2file=True)
+    
+    logger.info('ALL DONE!\n')
--- ML/gist/tf_gan_mnist.py
+++ ML/gist/tf_gan_mnist.py
@@ -14,43 +14,43 @@ from pydsutils.generic import create_logger
 logger = create_logger(__name__)
 
 def conv2d(x, W):
-    return tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding="SAME")
+    return tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def avg_pool_2x2(x):
     """Average pooling
     
     """
-    return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 def discriminator(x_image, reuse=False):
-    with tf.variable_scope("discriminator") as _:
+    with tf.variable_scope('discriminator') as _:
         if reuse:
             tf.get_variable_scope().reuse_variables()
         # First Conv and Pool Layers
-        W_conv1 = tf.get_variable("d_wconv1", [5, 5, 1, 8], 
+        W_conv1 = tf.get_variable('d_wconv1', [5, 5, 1, 8], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.02))
-        b_conv1 = tf.get_variable("d_bconv1", [8], initializer=tf.constant_initializer(0))
+        b_conv1 = tf.get_variable('d_bconv1', [8], initializer=tf.constant_initializer(0))
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = avg_pool_2x2(h_conv1)
 
         # Second Conv and Pool Layers
-        W_conv2 = tf.get_variable("d_wconv2", [5, 5, 8, 16], 
+        W_conv2 = tf.get_variable('d_wconv2', [5, 5, 8, 16], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.02))
-        b_conv2 = tf.get_variable("d_bconv2", [16], initializer=tf.constant_initializer(0))
+        b_conv2 = tf.get_variable('d_bconv2', [16], initializer=tf.constant_initializer(0))
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
         h_pool2 = avg_pool_2x2(h_conv2)
 
         # First Fully Connected Layer
-        W_fc1 = tf.get_variable("d_wfc1", [7 * 7 * 16, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        b_fc1 = tf.get_variable("d_bfc1", [32], initializer=tf.constant_initializer(0))
+        W_fc1 = tf.get_variable('d_wfc1', [7 * 7 * 16, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_fc1 = tf.get_variable('d_bfc1', [32], initializer=tf.constant_initializer(0))
         h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
         # Second Fully Connected Layer
-        W_fc2 = tf.get_variable("d_wfc2", [32, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        b_fc2 = tf.get_variable("d_bfc2", [1], initializer=tf.constant_initializer(0))
+        W_fc2 = tf.get_variable('d_wfc2', [32, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_fc2 = tf.get_variable('d_bfc2', [1], initializer=tf.constant_initializer(0))
 
         # Final Layer
         y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
@@ -64,7 +64,7 @@ def generator(z, batch_size, g_dim=64, c_dim=1, s=28, reuse=False):
     :param c_dim: Color dimension of output (MNIST is grayscale, so c_dim = 1 for us)
     :param s: Output size of the image
     """
-    with tf.variable_scope("generator") as _:
+    with tf.variable_scope('generator') as _:
         if reuse:
             tf.get_variable_scope().reuse_variables()
         # We want to slowly upscale the image, so the below values will help make that change gradual
@@ -75,47 +75,47 @@ def generator(z, batch_size, g_dim=64, c_dim=1, s=28, reuse=False):
 
         # First DeConv Layer
         output1_shape = [batch_size, s8, s8, g_dim*4]
-        W_conv1 = tf.get_variable("g_wconv1", [5, 5, output1_shape[-1], int(h0.get_shape()[-1])], 
+        W_conv1 = tf.get_variable('g_wconv1', [5, 5, output1_shape[-1], int(h0.get_shape()[-1])], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.1))
-        b_conv1 = tf.get_variable("g_bconv1", [output1_shape[-1]], initializer=tf.constant_initializer(.1))
+        b_conv1 = tf.get_variable('g_bconv1', [output1_shape[-1]], initializer=tf.constant_initializer(.1))
         H_conv1 = tf.nn.conv2d_transpose(h0, W_conv1, output_shape=output1_shape, strides=[1, 2, 2, 1], 
-                                         padding="SAME")
+                                         padding='SAME')
         H_conv1 = tf.contrib.layers.batch_norm(inputs = H_conv1, center=True, scale=True, is_training=True, 
-                                               scope="g_bn1")
+                                               scope='g_bn1')
         H_conv1 = tf.nn.relu(H_conv1)
         #Dimensions of H_conv1 = batch_size x 3 x 3 x 256
 
         # Second DeConv Layer
         output2_shape = [batch_size, s4 - 1, s4 - 1, g_dim*2]
-        W_conv2 = tf.get_variable("g_wconv2", [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])], 
+        W_conv2 = tf.get_variable('g_wconv2', [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.1))
-        b_conv2 = tf.get_variable("g_bconv2", [output2_shape[-1]], initializer=tf.constant_initializer(.1))
+        b_conv2 = tf.get_variable('g_bconv2', [output2_shape[-1]], initializer=tf.constant_initializer(.1))
         H_conv2 = tf.nn.conv2d_transpose(H_conv1, W_conv2, output_shape=output2_shape, strides=[1, 2, 2, 1], 
-                                         padding="SAME")
+                                         padding='SAME')
         H_conv2 = tf.contrib.layers.batch_norm(inputs = H_conv2, center=True, scale=True, is_training=True, 
-                                               scope="g_bn2")
+                                               scope='g_bn2')
         H_conv2 = tf.nn.relu(H_conv2)
         # Dimensions of H_conv2 = batch_size x 6 x 6 x 128
 
         # Third DeConv Layer
         output3_shape = [batch_size, s2 - 2, s2 - 2, g_dim*1]
-        W_conv3 = tf.get_variable("g_wconv3", [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])], 
+        W_conv3 = tf.get_variable('g_wconv3', [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.1))
-        b_conv3 = tf.get_variable("g_bconv3", [output3_shape[-1]], initializer=tf.constant_initializer(.1))
+        b_conv3 = tf.get_variable('g_bconv3', [output3_shape[-1]], initializer=tf.constant_initializer(.1))
         H_conv3 = tf.nn.conv2d_transpose(H_conv2, W_conv3, output_shape=output3_shape, strides=[1, 2, 2, 1], 
-                                         padding="SAME")
+                                         padding='SAME')
         H_conv3 = tf.contrib.layers.batch_norm(inputs = H_conv3, center=True, scale=True, is_training=True, 
-                                               scope="g_bn3")
+                                               scope='g_bn3')
         H_conv3 = tf.nn.relu(H_conv3)
         #Dimensions of H_conv3 = batch_size x 12 x 12 x 64
 
         #Fourth DeConv Layer
         output4_shape = [batch_size, s, s, c_dim]
-        W_conv4 = tf.get_variable("g_wconv4", [5, 5, output4_shape[-1], int(H_conv3.get_shape()[-1])], 
+        W_conv4 = tf.get_variable('g_wconv4', [5, 5, output4_shape[-1], int(H_conv3.get_shape()[-1])], 
                                   initializer=tf.truncated_normal_initializer(stddev=0.1))
-        b_conv4 = tf.get_variable("g_bconv4", [output4_shape[-1]], initializer=tf.constant_initializer(.1))
+        b_conv4 = tf.get_variable('g_bconv4', [output4_shape[-1]], initializer=tf.constant_initializer(.1))
         H_conv4 = tf.nn.conv2d_transpose(H_conv3, W_conv4, output_shape=output4_shape, strides=[1, 2, 2, 1], 
-                                         padding="VALID")
+                                         padding='VALID')
         H_conv4 = tf.nn.tanh(H_conv4)  # Dimensions of H_conv4 = batch_size x 28 x 28 x 1
     return H_conv4
 
@@ -123,16 +123,16 @@ def generator(z, batch_size, g_dim=64, c_dim=1, s=28, reuse=False):
 ##################################################################
 train_size = 55000
 g_dimensions = 100
-logger.info("Program started")
+logger.info('Program started')
 
 # Data
-mnist = input_data.read_data_sets("MNIST_data/")
-logger.info("Successfully loaded raw MNIST data")
+mnist = input_data.read_data_sets('MNIST_data/')
+logger.info('Successfully loaded raw MNIST data')
 
 x_train = mnist.train.images[:train_size,:] 
-logger.info("Shapre of x_train: %s " % str(x_train.shape))
+logger.info('Shapre of x_train: %s ' % str(x_train.shape))
 image = x_train[random.randint(0, train_size)].reshape([28, 28])
-plt.imshow(image, cmap=plt.get_cmap("gray_r"))
+plt.imshow(image, cmap=plt.get_cmap('gray_r'))
 plt.show()
 
 # Generator is untrained, so expect a bad result
@@ -152,7 +152,7 @@ plt.show()
 batch_size = 16
 tf.reset_default_graph()  # Since we changed our batch size (from 1 to 16), need to reset TF graph
 sess = tf.Session()
-d_placeholder = tf.placeholder("float", shape = [None, 28, 28, 1])  # Placeholder for input images to discriminator
+d_placeholder = tf.placeholder('float', shape = [None, 28, 28, 1])  # Placeholder for input images to discriminator
 g_placeholder = tf.placeholder(tf.float32, [None, g_dimensions])  # Placeholder for input noise vectors to generator
 
 # Dx will hold discriminator outputs (unnormalized) for the real MNIST images
@@ -168,8 +168,8 @@ d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, 
 d_loss = d_loss_real + d_loss_fake
 
 tvars = tf.trainable_variables()
-d_vars = [var for var in tvars if "d_" in var.name]
-g_vars = [var for var in tvars if "g_" in var.name]
+d_vars = [var for var in tvars if 'd_' in var.name]
+g_vars = [var for var in tvars if 'g_' in var.name]
 
 with tf.variable_scope(tf.get_variable_scope(), reuse=False):
     d_train_optimizer = tf.train.AdamOptimizer().minimize(d_loss, var_list=d_vars)
@@ -186,7 +186,7 @@ for iteration in range(iterations):
                         d_placeholder: real_image_batch}) 
     _, _ = sess.run([g_train_optimizer, g_loss], feed_dict={g_placeholder:g_batch})  # Update the generator
     if iteration % 10 == 0:
-        logger.info("Successfully completed iteration %5d out of %d" %(iteration, iterations - 1))
+        logger.info('Successfully completed iteration %5d out of %d' %(iteration, iterations - 1))
 
 # A sample image looks like after training.
 sample_image = generator(g_placeholder, 1, g_dimensions, reuse=True)
@@ -197,4 +197,4 @@ plt.imshow(my_i, cmap='gray_r')
 
 # Finalize
 sess.close()
-logger.info("ALL DONE!\n")
+logger.info('ALL DONE!\n')
