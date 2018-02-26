df4edd13c119f9bcbf398ac074396248b0cf1a60
Xin Heng <xin.heng@gmail.com>
2018-02-25 20:44:10 -0800

    Added a simple model to demo gresearch and pymlearn

---
+++ ML/g_research/scripts/setenv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Define environment variables
+
+clustername=`echo $HOSTNAME | awk '{print substr($0, 0, 7)}'`
+
+export SPARK_HOME=/usr/local/Cellar/apache-spark/2.2.1/libexec
+export PYSPARK_PYTHON=`which python`
+
+export PYTHONPATH="$(dirname "$(pwd)")":$PYTHONPATH  # Append the parent of the project root
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
+
+printf "SPARK_HOME:     $SPARK_HOME\n"
+printf "PYSPARK_PYTHON: $PYSPARK_PYTHON\n"
+printf "PYTHONPATH:     $PYTHONPATH\n"
+printf "setenv.sh - successfully completed\n\n"
