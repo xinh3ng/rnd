dc80154095ddca1ef5d73d69dbd195ff52782a06
Xin Heng <xheng@punchh.com>
2019-05-04 15:28:52 -0700

    Added pytorch multi sentiment example

--- AI/gist/nlp/pytorch_nlp_multi_sentiment.py
+++ AI/gist/nlp/pytorch_nlp_multi_sentiment.py
@@ -1,11 +1,168 @@
 """
 """
 from pdb import set_trace as debug
-import os
-import sqlite3
-import pandas as pd
-import re
-
 from pypchutils.generic import create_logger
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torchtext import data
+from torchtext import datasets
+import random
 
 logger = create_logger(__name__, level="info")
+
+SEED = 1234
+MAX_VOCAB_SIZE = 25_000
+BATCH_SIZE = 64
+
+
+class CNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout, pad_idx):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(in_channels=1, out_channels=n_filters, kernel_size=(fs, embedding_dim)) for fs in filter_sizes]
+        )
+
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+        # text = [sent len, batch size]
+        text = text.permute(1, 0)
+        # text = [batch size, sent len]
+
+        embedded = self.embedding(text)
+        # embedded = [batch size, sent len, emb dim]
+        embedded = embedded.unsqueeze(1)
+        # embedded = [batch size, 1, sent len, emb dim]
+
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+
+        # conv_n = [batch size, n_filters, sent len - filter_sizes[n]]
+
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        # pooled_n = [batch size, n_filters]
+
+        cat = self.dropout(torch.cat(pooled, dim=1))
+        # cat = [batch size, n_filters * len(filter_sizes)]
+
+        return self.fc(cat)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+
+def categorical_accuracy(preds, y):
+    """Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    max_preds = preds.argmax(dim = 1, keepdim = True)  # get the index of the max probability
+    correct = max_preds.squeeze(1).eq(y)
+    return correct.sum() / torch.FloatTensor([y.shape[0]])
+
+
+def train(model, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+    for batch in iterator:
+        optimizer.zero_grad()
+        predictions = model(batch.text)
+
+        loss = criterion(predictions, batch.label)
+        acc = categorical_accuracy(predictions, batch.label)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.text)
+
+            loss = criterion(predictions, batch.label)
+
+            acc = categorical_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+#
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(tokenize="spacy")
+LABEL = data.LabelField()
+
+# Create train, validate and test data sets
+train_data, test_data = datasets.TREC.splits(TEXT, LABEL, fine_grained=False)
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+logger.info("Successfully split the train, validate and test data sets")
+
+# Build the vocabulary, do not need to set a max_size
+
+
+TEXT.build_vocab(train_data, max_size=MAX_VOCAB_SIZE, vectors="glove.6B.100d", unk_init=torch.Tensor.normal_)
+LABEL.build_vocab(train_data)
+logger.info("Successfully built the vocabulary for TEXT and LABEL")
+
+logger.info("The labels: %s" % (LABEL.vocab.stoi))
+
+# Set up the iterator
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), batch_size=BATCH_SIZE, device=device
+)
+
+
+# CNN model parameters
+INPUT_DIM = len(TEXT.vocab)
+EMBEDDING_DIM = 100
+N_FILTERS = 100
+FILTER_SIZES = [2, 3, 4]
+OUTPUT_DIM = len(LABEL.vocab)
+DROPOUT = 0.5
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
+print(f"The model has {count_parameters(model):,} trainable parameters")
+
+# Load our pre-trained embeddings.
+pretrained_embeddings = TEXT.vocab.vectors
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.CrossEntropyLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+#
+logger.info("ALL DONE\n")
