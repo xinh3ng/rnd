bdf81e26efb9a6088f8c15ebde77bd9f7ebd014f
Xin Heng <xheng@punchh.com>
2020-04-15 23:14:20 -0700

    Small update

--- AI/cv/detection/engine.py
+++ AI/cv/detection/engine.py
@@ -28,13 +28,11 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         loss_dict = model(images, targets)
-
         losses = sum(loss for loss in loss_dict.values())
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
         loss_value = losses_reduced.item()
 
         if not math.isfinite(loss_value):
@@ -45,13 +43,11 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         optimizer.zero_grad()
         losses.backward()
         optimizer.step()
-
         if lr_scheduler is not None:
             lr_scheduler.step()
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
     return metric_logger
 
 
--- AI/cv/pytorch_object_maskrcnn.py
+++ AI/cv/pytorch_object_maskrcnn.py
@@ -123,6 +123,9 @@ def main(root: str, num_classes: int = 2, epochs: int = 10):
     """
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+    ################
+    # Set up the data
+    ################
     dataset = PennFudanDataset(root, get_transform(train=True))
     dataset_test = PennFudanDataset(root, get_transform(train=False))
 
@@ -138,16 +141,18 @@ def main(root: str, num_classes: int = 2, epochs: int = 10):
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
     )
-
+    
+    ################
+    # Set up the model
+    ################
     model = get_model_instance_segmentation(num_classes)
     model.to(device)
     logger.info("Successfully loaded the model")
 
-    # construct an optimizer, and a learning rate scheduler
+    # Construct an optimizer, and a learning rate scheduler
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
     for epoch in range(epochs):
         train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
         lr_scheduler.step()  # update the learning rate
