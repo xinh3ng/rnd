fb7e7c9d579686586dfeaa1a1caf2aee2161af1a
Xin Heng <xheng@punchh.com>
2020-04-25 11:22:29 -0700

    Cmpleted the script

--- AI/nlp/pytorch_speech_recog.py
+++ AI/nlp/pytorch_speech_recog.py
@@ -3,14 +3,15 @@
 https://www.assemblyai.com/blog/end-to-end-speech-recognition-pytorch
 
 """
+from comet_ml import Experiment
 import json
 import os
-import pandas as pd
 from pypchutils.generic import create_logger
 import torch
-import torchaudio
 import torch.nn as nn
-import torch.optim as optim
+from torch.nn import functional as F
+from torch.utils import data
+import torchaudio
 
 logger = create_logger(__name__)
 
@@ -19,6 +20,76 @@ def download_data(folder: str = "{os.environ.get('HOME')}/Google Drive/xheng/dat
     train_dataset = torchaudio.datasets.LIBRISPEECH(folder, url="train-clean-100", download=download)
     test_dataset = torchaudio.datasets.LIBRISPEECH(folder, url="test-clean", download=download)
     return train_dataset, test_dataset
+
+
+def wer(reference, hypothesis, ignore_case=False, delimiter=" "):
+    """Calculate word error rate (WER). WER compares reference text and
+    hypothesis text in word-level. WER is defined as:
+    .. math::
+        WER = (Sw + Dw + Iw) / Nw
+    where
+    .. code-block:: text
+        Sw is the number of words subsituted,
+        Dw is the number of words deleted,
+        Iw is the number of words inserted,
+        Nw is the number of words in the reference
+    We can use levenshtein distance to calculate WER. Please draw an attention
+    that empty items will be removed when splitting sentences by delimiter.
+    :param reference: The reference sentence.
+    :type reference: basestring
+    :param hypothesis: The hypothesis sentence.
+    :type hypothesis: basestring
+    :param ignore_case: Whether case-sensitive or not.
+    :type ignore_case: bool
+    :param delimiter: Delimiter of input sentences.
+    :type delimiter: char
+    :return: Word error rate.
+    :rtype: float
+    :raises ValueError: If word number of reference is zero.
+    """
+    edit_distance, ref_len = word_errors(reference, hypothesis, ignore_case, delimiter)
+
+    if ref_len == 0:
+        raise ValueError("Reference's word number should be greater than 0.")
+
+    wer = float(edit_distance) / ref_len
+    return wer
+
+
+def cer(reference, hypothesis, ignore_case=False, remove_space=False):
+    """Calculate charactor error rate (CER). CER compares reference text and
+    hypothesis text in char-level. CER is defined as:
+    .. math::
+        CER = (Sc + Dc + Ic) / Nc
+    where
+    .. code-block:: text
+        Sc is the number of characters substituted,
+        Dc is the number of characters deleted,
+        Ic is the number of characters inserted
+        Nc is the number of characters in the reference
+    We can use levenshtein distance to calculate CER. Chinese input should be
+    encoded to unicode. Please draw an attention that the leading and tailing
+    space characters will be truncated and multiple consecutive space
+    characters in a sentence will be replaced by one space character.
+    :param reference: The reference sentence.
+    :type reference: basestring
+    :param hypothesis: The hypothesis sentence.
+    :type hypothesis: basestring
+    :param ignore_case: Whether case-sensitive or not.
+    :type ignore_case: bool
+    :param remove_space: Whether remove internal space characters
+    :type remove_space: bool
+    :return: Character error rate.
+    :rtype: float
+    :raises ValueError: If the reference length is zero.
+    """
+    edit_distance, ref_len = char_errors(reference, hypothesis, ignore_case, remove_space)
+
+    if ref_len == 0:
+        raise ValueError("Length of reference should be greater than 0.")
+
+    cer = float(edit_distance) / ref_len
+    return cer
 
 
 class TextTransform:
@@ -82,14 +153,305 @@ class TextTransform:
         return "".join(string).replace("", " ")
 
 
+def data_processing(data, text_transform, train_or_test: str = "train"):
+
+    # Set up the audio transforms for train and validation sets
+    train_audio_transforms = nn.Sequential(
+        torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
+        torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
+        torchaudio.transforms.TimeMasking(time_mask_param=35),
+    )
+    valid_audio_transforms = torchaudio.transforms.MelSpectrogram()
+
+    spectrograms, labels, input_lengths, label_lengths = [], [], [], []
+    for (waveform, _, utterance, _, _, _) in data:
+        if train_or_test == "train":
+            spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        elif train_or_test == "valid":
+            spec = valid_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+
+        spectrograms.append(spec)
+        label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
+        labels.append(label)
+        input_lengths.append(spec.shape[0] // 2)
+        label_lengths.append(len(label))
+
+    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+    return spectrograms, labels, input_lengths, label_lengths
+
+
+class CNNLayerNorm(nn.Module):
+    """Layer normalization built for cnns input"""
+
+    def __init__(self, n_feats):
+        super(CNNLayerNorm, self).__init__()
+        self.layer_norm = nn.LayerNorm(n_feats)
+
+    def forward(self, x):
+        # x (batch, channel, feature, time)
+        x = x.transpose(2, 3).contiguous()  # (batch, channel, time, feature)
+        x = self.layer_norm(x)
+        return x.transpose(2, 3).contiguous()  # (batch, channel, feature, time)
+
+
+class ResidualCNN(nn.Module):
+    """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf except with layer norm instead of batch norm
+    """
+
+    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats):
+        super(ResidualCNN, self).__init__()
+
+        self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel // 2)
+        self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel // 2)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.layer_norm1 = CNNLayerNorm(n_feats)
+        self.layer_norm2 = CNNLayerNorm(n_feats)
+
+    def forward(self, x):
+        residual = x  # (batch, channel, feature, time)
+        x = self.layer_norm1(x)
+        x = F.gelu(x)
+        x = self.dropout1(x)
+        x = self.cnn1(x)
+        x = self.layer_norm2(x)
+        x = F.gelu(x)
+        x = self.dropout2(x)
+        x = self.cnn2(x)
+        x += residual
+        return x  # (batch, channel, feature, time)
+
+
+class BidirectionalGRU(nn.Module):
+    def __init__(self, rnn_dim, hidden_size, dropout, batch_first):
+        super(BidirectionalGRU, self).__init__()
+        self.BiGRU = nn.GRU(
+            input_size=rnn_dim, hidden_size=hidden_size, num_layers=1, batch_first=batch_first, bidirectional=True
+        )
+        self.layer_norm = nn.LayerNorm(rnn_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = F.gelu(x)
+        x, _ = self.BiGRU(x)
+        x = self.dropout(x)
+        return x
+
+
+class SpeechRecognitionModel(nn.Module):
+    """Speech Recognition Model Inspired by DeepSpeech 2"""
+
+    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
+        super(SpeechRecognitionModel, self).__init__()
+        n_feats = n_feats // 2
+        self.cnn = nn.Conv2d(1, 32, 3, stride=stride, padding=3 // 2)  # cnn for extracting hierarchical features
+
+        # n residual cnn layers with filter size of 32
+        self.rescnn_layers = nn.Sequential(
+            *[ResidualCNN(32, 32, kernel=3, stride=1, dropout=dropout, n_feats=n_feats) for _ in range(n_cnn_layers)]
+        )
+        self.fully_connected = nn.Linear(n_feats * 32, rnn_dim)
+        self.birnn_layers = nn.Sequential(
+            *[
+                BidirectionalGRU(
+                    rnn_dim=rnn_dim if i == 0 else rnn_dim * 2, hidden_size=rnn_dim, dropout=dropout, batch_first=i == 0
+                )
+                for i in range(n_rnn_layers)
+            ]
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(rnn_dim * 2, rnn_dim),  # birnn returns rnn_dim*2
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(rnn_dim, n_class),
+        )
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.rescnn_layers(x)
+        sizes = x.size()
+        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
+        x = x.transpose(1, 2)  # (batch, time, feature)
+        x = self.fully_connected(x)
+        x = self.birnn_layers(x)
+        x = self.classifier(x)
+        return x
+
+
+class IterMeter(object):
+    """keeps track of total iterations"""
+
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
+
+
+def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment):
+    model.train()
+    data_len = len(train_loader.dataset)
+    with experiment.train():
+        for batch_idx, _data in enumerate(train_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(spectrograms)  # (batch, time, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)  # (time, batch, n_class)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+            loss.backward()
+
+            experiment.log_metric("loss", loss.item(), step=iter_meter.get())
+            experiment.log_metric("learning_rate", scheduler.get_lr(), step=iter_meter.get())
+
+            optimizer.step()
+            scheduler.step()
+            iter_meter.step()
+            if batch_idx % 100 == 0 or batch_idx == data_len:
+                logger.info(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(spectrograms),
+                        data_len,
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
+
+
+def test(model, device, test_loader, criterion, iter_meter, experiment, text_transform):
+    logger.info("\nevaluating…")
+    model.eval()
+    test_loss = 0
+    test_cer, test_wer = [], []
+    with experiment.test():
+        with torch.no_grad():
+            for I, _data in enumerate(test_loader):
+                spectrograms, labels, input_lengths, label_lengths = _data
+                spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+                output = model(spectrograms)  # (batch, time, n_class)
+                output = F.log_softmax(output, dim=2)
+                output = output.transpose(0, 1)  # (time, batch, n_class)
+
+                loss = criterion(output, labels, input_lengths, label_lengths)
+                test_loss += loss.item() / len(test_loader)
+
+                decoded_preds, decoded_targets = GreedyDecoder(
+                    text_transform, output.transpose(0, 1), labels, label_lengths
+                )
+                for j in range(len(decoded_preds)):
+                    test_cer.append(cer(decoded_targets[j], decoded_preds[j]))
+                    test_wer.append(wer(decoded_targets[j], decoded_preds[j]))
+
+    avg_cer = sum(test_cer) / len(test_cer)
+    avg_wer = sum(test_wer) / len(test_wer)
+    experiment.log_metric("test_loss", test_loss, step=iter_meter.get())
+    experiment.log_metric("cer", avg_cer, step=iter_meter.get())
+    experiment.log_metric("wer", avg_wer, step=iter_meter.get())
+
+    logger.info(
+        "Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n".format(test_loss, avg_cer, avg_wer)
+    )
+
+
+def GreedyDecoder(text_transform, output, labels, label_lengths, blank_label=28, collapse_repeated=True):
+    arg_maxes = torch.argmax(output, dim=2)
+    decodes = []
+    targets = []
+    for i, args in enumerate(arg_maxes):
+        decode = []
+        targets.append(text_transform.int_to_text(labels[i][: label_lengths[i]].tolist()))
+        for j, index in enumerate(args):
+            if index != blank_label:
+                if collapse_repeated and j != 0 and index == args[j - 1]:
+                    continue
+                decode.append(index.item())
+        decodes.append(text_transform.int_to_text(decode))
+    return decodes, targets
+
+
 def main(
     data_folder: str = "{os.environ.get('HOME')}/Google Drive/xheng/data",
     download: bool = False,
-    batch_size: int = 100,
-    epochs: int = 20,
-    learning_rate=0.01,
+    learning_rate: float = 5e-4,
+    batch_size: int = 20,
+    epochs: int = 10,
+    experiment=Experiment(api_key=os.environ.get("API_KEY"), disabled=True),
 ):
+
+    hparams = {
+        "n_cnn_layers": 3,
+        "n_rnn_layers": 5,
+        "rnn_dim": 512,
+        "n_class": 29,
+        "n_feats": 128,
+        "stride": 2,
+        "dropout": 0.1,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "epochs": epochs,
+    }
+    experiment.log_parameters(hparams)
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    text_transform = TextTransform()
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
     train_dataset, test_dataset = download_data(folder=data_folder, download=download)
+    train_loader = data.DataLoader(
+        dataset=train_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=True,
+        collate_fn=lambda x: data_processing(x, text_transform, "train"),
+        **kwargs,
+    )
+    test_loader = data.DataLoader(
+        dataset=test_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=False,
+        collate_fn=lambda x: data_processing(x, text_transform, "valid"),
+        **kwargs,
+    )
+
+    model = SpeechRecognitionModel(
+        hparams["n_cnn_layers"],
+        hparams["n_rnn_layers"],
+        hparams["rnn_dim"],
+        hparams["n_class"],
+        hparams["n_feats"],
+        hparams["stride"],
+        hparams["dropout"],
+    ).to(device)
+    logger.info(model)
+    logger.info("No. of Model Parameters: %d" % sum([param.nelement() for param in model.parameters()]))
+
+    optimizer = torch.optim.AdamW(model.parameters(), hparams["learning_rate"])
+    criterion = nn.CTCLoss(blank=28).to(device)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=hparams["learning_rate"],
+        steps_per_epoch=int(len(train_loader)),
+        epochs=hparams["epochs"],
+        anneal_strategy="linear",
+    )
+
+    iter_meter = IterMeter()
+    for epoch in range(epochs):
+        train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
+        test(model, device, test_loader, criterion, iter_meter, experiment, text_transform)
+    return
 
 
 if __name__ == "__main__":
