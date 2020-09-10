5e2615f0084ce4d40fb4e312692455836b3f6fd2
Xin Heng <xheng@punchh.com>
2020-09-09 20:47:22 -0700

    Added a cortex labs example

--- eng/go/src/master_go_udemy/select.go
+++ eng/go/src/master_go_udemy/select.go
@@ -18,7 +18,7 @@ func findsc(name, server string, c chan int) {
 	// Simulate a searching action
 	time.Sleep(time.Duration(rand.Intn(50)) * time.Minute)
 
-	// Return security clearance from someone
+	// Return security clearance level of name
 	c <- scMapping[name]
 }
 
@@ -29,11 +29,21 @@ func main() {
 
 	c1 := make(chan int)
 	c2 := make(chan int)
-	
-	name := "James"
+	name := "James" // The name to be searched
 
 	go findsc(name, "Server 1", c1)
 	go findsc(name, "Server 2", c2)
+
+	select {
+	case sc := <-c1:
+		fmt.Println(name, "has a security clearance level of ", sc, " found from Server 1")
+	
+	case sc := <-c2:
+		fmt.Println(name, "has a security clearance level of ", sc, " found from Server 2")
+	 
+	case <-time.After(100 * time.Millisecond):
+		fmt.Println("Search has timed out")
+	}
 
 	fmt.Println("\nALL DONE!")
 }
