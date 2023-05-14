c6e49bca38f6f86b2dabcee3dd2770900898f92d
Xin Heng <xin.heng@gmail.com>
2023-05-13 17:51:10 -0700

    Test code of download video and write a summary

--- AI/chatgpt/video_summary.py
+++ AI/chatgpt/video_summary.py
@@ -1,15 +1,78 @@
 """
 
-# Links
-
 """
+from bs4 import BeautifulSoup
 import json
-from totepyutils.generic import create_logger
+from moviepy import editor as E
+import openai
+import os
+from pytube import YouTube
+import requests
+from totepy.generic import create_logger
 
 logger = create_logger(__name__, level="info")
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.organization_id = "org-bSEDQevVUvT2CCljpD3DNAEA"
 
-def main(**kwargs):
+
+def youtube_download(url):
+    youtube = YouTube(url)
+    youtube = youtube.streams.get_highest_resolution()
+    logger.info(f"Starting download from {url}")
+    try:
+        youtube.download()
+        logger.info("Successfully completed the download")
+    except:
+        logger.error("An error has occurred")
+
+
+def download(url, video_filepath):
+    response = requests.get(url, stream=True)
+    with open(video_filepath, "wb") as f:
+        for chunk in response.iter_content(chunk_size=256):
+            f.write(chunk)
+
+
+def main(
+    video_filepath: str = "How it Happened - The 2008 Financial Crisis Crash Course Economics 12.mp4",
+    verbose: int = 1,
+):
+
+    if 0 == 1:
+        download_url = "https://www.youtube.com/watch?v=GPOv72Awo68&ab_channel=CrashCourse"
+        video_filepath = "How it Happened - The 2008 Financial Crisis Crash Course Economics 12.mp4"
+        audio_filepath = video_filepath.replace("mp4", "mp3")
+        youtube_download(download_url)
+
+    if 1 == 1:
+        download_url = "https://v-cf.caffeine.tv/v1/E3E72733E4C84D8F91DF91D37E346398/abd85fae-8e1c-400a-8385-f4df54db045c/primary/chunk-stream_0-00041.m4s"
+        video_filepath = "demo.mp4"
+        audio_filepath = video_filepath.replace("mp4", "mp3")
+        download(download_url, video_filepath)
+
+    logger.info("Converting mp4 to mp3 ...")
+    video = E.VideoFileClip(video_filepath)
+    video.audio.write_audiofile(audio_filepath)
+
+    if 0 == 1:
+        logger.info("Using whisper-1 to get the transcription as text ...")
+        transcript = openai.Audio.transcribe("whisper-1", open(audio_filepath, "rb"))
+        text = transcript["text"]
+        logger.info(text)
+
+    logger.info("Calling chatGPT...")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.",
+            },
+            {"role": "user", "content": f"Can you kindly summarize the following text in about 20 words: {text}"},
+        ],
+    )
+    logger.info("Reply: \n%s" % response["choices"][0]["message"]["content"])
     return
 
 
@@ -17,9 +80,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_episodes", type=int, default=100)
+    parser.add_argument("--verbose", type=int, default=1)
     args = vars(parser.parse_args())
     logger.info("Cmd line args:\n{}".format(json.dumps(args, sort_keys=True, indent=4)))
 
     main(**args)
-    logger.info("\nALL DONE!\n")
+    logger.info("ALL DONE!\n")
