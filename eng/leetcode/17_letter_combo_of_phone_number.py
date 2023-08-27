56f7e465fbe66f3509ff8465f3ee0b5d20eb760c
xinheng <97647478+xinhengcaffeinetv@users.noreply.github.com>
2023-08-27 09:57:34 -0700

    linting

--- eng/leetcode/17_letter_combo_of_phone_number.py
+++ eng/leetcode/17_letter_combo_of_phone_number.py
@@ -1,9 +1,28 @@
 """
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+Constraints:
+
+0 <= digits.length <= 4
+
+digits[i] is a digit in the range ['2', '9'].
+
 """
 from typing import List
 
 
 class Solution:
+    digits_to_letters = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"],
+    }
+
     def letterCombinations(self, digits: str) -> List[str]:
         return
 
@@ -20,6 +39,9 @@ inputs = {
 solution = Solution()
 for digits, out in inputs.items():
     print("####################")
+
+    assert "1" not in digits, f"digits: {digits} containt '1'"
+    assert len(digits) <= 4
 
     out = solution.letterCombinations(digits)
     print(f"digits: {digits}, out: {out}")
